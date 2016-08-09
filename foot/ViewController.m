65309866469fc92b7ecc6dfe7cb25c89691b1703
Admin <1033948449@qq.com>
2016-08-09 10:53:06 +0800

    Initial Commit

---
+++ foot/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  foot
+//
+//  Created by Admin on 16/8/9.
+//  Copyright © 2016年 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
